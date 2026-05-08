56598168ffaea2a7ca7a2db4cf41b58a42ddc7d0
Krutarth Pipaliya <krutarth.pipaliya@simformsolutions.com>
2026-05-08 15:50:39 +0530

    fix: alert issue fixed

--- src/utility/cellClick.ts
+++ src/utility/cellClick.ts
@@ -44,7 +44,9 @@ export function cellClick(ele: HTMLDivElement) {
                 ele.classList.add("correct-select");
                 found = true;
                 if (set.size === 7) {
-                    alert(`player ${opponent === 1 ? 2 : 1} Won`);
+                    setTimeout(() =>
+                        alert(`player ${opponent === 1 ? 2 : 1} Won`),
+                    );
                 }
                 break;
             }
